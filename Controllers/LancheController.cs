cc01b84053917c3bc21c638ac34b4d7c21e790dd
Luis Guilherme <51214414+Becero@users.noreply.github.com>
2023-11-01 13:50:44 -0300

    Menu 2

--- Controllers/LancheController.cs
+++ Controllers/LancheController.cs
@@ -31,25 +31,35 @@ namespace Lancheounet.Controllers
             }
             else
             {
-                if (string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase))//ignora caixa alta ou baixa
-                {
-                    lanches = _lancheRepository.Lanches.Where(l => l.Categoria.CategoriaNome.Equals("Normal"))
-                        .OrderBy(l => l.Nome);
-                }
-                else
-                {
+                //if (string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase))//ignora caixa alta ou baixa
+                //{
+                //    lanches = _lancheRepository.Lanches.Where(l => l.Categoria.CategoriaNome.Equals("Normal"))
+                //        .OrderBy(l => l.Nome);
+                //}
+                //else
+                //{
 
-                    lanches = _lancheRepository.Lanches.Where(l => l.Categoria.CategoriaNome.Equals("Natural"))
-                        .OrderBy(l => l.Nome);
-                }
+                //    lanches = _lancheRepository.Lanches.Where(l => l.Categoria.CategoriaNome.Equals("Natural"))
+                //        .OrderBy(l => l.Nome);
+                //}
+                lanches = _lancheRepository.Lanches
+                    .Where(l => l.Categoria.CategoriaNome.Equals(categoria))
+                    .OrderBy(c => c.Nome);
                 categoriaAtual = categoria;
             }
+
             var lanchesListViewModel = new LancheListViewModel 
             { 
                 Lanches = lanches,
                 CategoriaAtual=categoriaAtual,
-            };
+            }; 
+            
             return View(lanchesListViewModel);
+        }
+        public IActionResult Details(int lancheId)
+        {
+            var lanche = _lancheRepository.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
+            return View(lanche);
         }
     }
 }
